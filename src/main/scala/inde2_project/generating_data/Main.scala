9200f27560178f638d3fded1d131c98cf6bede83
Theo Tinti <theo.tinti@epita.fr>
2023-06-27 01:57:44 +0200

    Separate producer and consumer idk how it works

--- src/main/scala/inde2_project/generating_data/Main.scala
+++ src/main/scala/inde2_project/generating_data/Main.scala
@@ -17,7 +17,32 @@ import faker._
 
 
 object Main {
-    def generateData(phrases: List[String], producer: KafkaProducer[String, String]) = {
+    /* Generate json containing ID, location, list of persons, list of words, timestamp :
+    {
+      peacewatcher_id: 1,
+      location: {
+        latitude: 0.0,
+        longitude: 0.0
+      },
+      persons: [
+        {
+          name: "John Doe",
+          harmonyScore: 0.5
+        },
+        ...
+      ],
+      words: [
+        "peace",
+        "love",
+        "harmony",
+        ...
+      ],
+      timestamp: "2021-04-01T12:00:00.000"
+    }
+    On envoie ces json sur le topic "inde2_project" du broker Kafka
+    */
+    def generateData(producer: KafkaProducer[String, String]) = {
+        val random_words = List("peace", "love", "harmony", "happiness", "joy", "respect", "tolerance", "solidarity", "equality", "freedom", "justice", "dignity", "non-violence", "non-violent", "nonviolence", "violent", "violence", "hello", "hi", "goodbye", "bye", "good morning", "good afternoon") 
         (0 to 1000).foreach((i) => {
             val event = Report(
                 Random.between(0, 10000),
@@ -27,7 +52,7 @@ object Main {
                             name = Faker.default.fullName(),
                             harmonyScore = Random.nextDouble()
                         )).toList,
-                (1 to Random.between(1, 10)).map(p => phrases(Random.nextInt(phrases.size))).toList,
+                (1 to Random.between(1, 10)).map(p => random_words(Random.nextInt(random_words.size))).toList,
                 LocalDateTime.now()
             )
             val eventJsonString = Json.stringify(Json.toJson(event))
@@ -46,32 +71,7 @@ object Main {
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
     val producer : KafkaProducer[String, String] = new KafkaProducer[String, String](props)
 
-    val phrases = List("peace", "love", "harmony", "happiness", "joy", "respect", "tolerance", "solidarity", "equality", "freedom", "justice", "dignity", "non-violence", "non-violent", "nonviolence", "violent", "violence", "hello", "hi", "goodbye", "bye", "good morning", "good afternoon")
-    generateData(phrases, producer)
-    println("GENERATING AND SENDING DATA DONE")
+    generateData(producer)
     producer.close()
-
-    val props2 = new Properties()
-    props2.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092") // Adresse et port du broker Kafka
-    props2.put(ConsumerConfig.GROUP_ID_CONFIG, "drone-consumer-group") // ID du groupe de consommateurs
-    props2.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer]) // Désérialiseur de clés
-    props2.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer]) // Désérialiseur de valeurs
-
-    val consumer = new KafkaConsumer[String, String](props2)
-    println("Subscribing to data")
-    consumer.subscribe(java.util.Collections.singletonList("inde2_project"))
-    println("Getting data Done")
-
-    while (true) {
-      println("printing data")
-      val records = consumer.poll(100) // Récupère les enregistrements du topic
-      for (record <- records.asScala) {
-        val key = record.key()
-        val value = record.value()
-        println(s"Clé: $key, Valeur: $value")
-      }
-    }
-
-    consumer.close()
     }
 }
